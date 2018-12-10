9241eaa4c670abe7e0f47d310cd0253c063958ed
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-09 23:48:16 -0200

    add graphql endpoint and db session teardown

--- src/infra/server.py
+++ src/infra/server.py
@@ -1,7 +1,18 @@
 from flask import Flask
+from flask_graphql import GraphQLView
+
+from src.infra.database import db_session
+from src.delivery.schema import schema
 
 
 app = Flask(__name__)
+
+app.add_url_rule('/delivery',
+                 view_func=GraphQLView.as_view('delivery', schema=schema, graphiql=True))
+
+@app.teardown_appcontext
+def remove_session(exception=None):
+    db_session.remove()
 
 if __name__ == '__main__':
     app.run(debug=True)
