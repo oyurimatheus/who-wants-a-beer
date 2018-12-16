e55b23478dff0b9a0d5685040aa6bbce488b165c
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-16 02:18:45 -0200

    changes the data base ip

--- src/infra/database.py
+++ src/infra/database.py
@@ -3,8 +3,7 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-
-engine = create_engine('postgresql://app:app@127.0.0.1:5432/whowantsabeer')
+engine = create_engine('postgresql://app:app@172.16.0.2:5432/whowantsabeer')
 meta_data = MetaData(bind=engine)
 
 session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -12,6 +11,3 @@ db_session = scoped_session(session_factory)
 
 Base = declarative_base(bind=engine, metadata=meta_data)
 Base.query = db_session.query_property()
-
-if __name__ == '__main__':
-    meta_data.create_all()
