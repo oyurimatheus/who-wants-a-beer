857ec6222079ea1e2446d466168ae6ab96ef8781
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-17 00:01:25 -0200

    extract to use docker envs variables

--- src/infra/database.py
+++ src/infra/database.py
@@ -3,7 +3,17 @@ from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-engine = create_engine('postgresql://app:app@172.16.0.2:5432/whowantsabeer')
+import os
+
+
+DATABASE_USER = os.getenv('DATABASE_USER', 'app')
+DATABASE_PASSWORD = os.getenv('DATABASE_PASSWORD', 'app')
+DATABASE_HOST = os.getenv('DATABASE_HOST', '127.0.0.1')
+DATABASE_PORT = os.getenv('DATABASE_PORT', '5432')
+DATABASE_NAME = os.getenv('DATABASE_NAME', 'whowantsabeer')
+
+url = f'postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}'
+engine = create_engine(url)
 meta_data = MetaData(bind=engine)
 
 session_factory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
