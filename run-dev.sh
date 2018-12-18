f592494d4991912c1b84cb4fb95248587a57f801
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-18 00:27:01 -0200

    fix docker command

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,4 @@
-docker run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --name whowantsabeer -d mdillon/postgis
+docker network create -d bridge --subnet 172.16.0.0/24 --gateway 172.16.0.1 containers
+docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeers --net=containers --name whowantsabeer -d mdillon/postgis
 python -m src.infra.create_db.py
 python -m src.infra.server.py
