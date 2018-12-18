cf87280e0ef8b94e617eddab53dcc6e91a078b7a
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-18 19:41:24 -0200

    update run file

--- run-dev.sh
+++ run-dev.sh
@@ -1,5 +1,7 @@
-docker network create -d bridge --subnet 172.16.0.0/24 --gateway 172.16.0.1 beer
-docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=beer --name whowantsabeer -d mdillon/postgis
+#!/bin/bash
+
+docker network create -d bridge --subnet 172.16.0.0/24 --gateway 172.16.0.1 beer-dev
+docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=beer-dev --name whowantsabeer -d mdillon/postgis
 sleep 10
 python -m src.infra.create_db
 python -m tests.generate_db_data
