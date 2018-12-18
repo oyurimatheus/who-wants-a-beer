3c6de1e28fe35ad3c9ece063948c80d5101141d5
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-18 01:15:10 -0200

    remove python extension from file

--- run-dev.sh
+++ run-dev.sh
@@ -1,4 +1,5 @@
 docker network create -d bridge --subnet 172.16.0.0/24 --gateway 172.16.0.1 containers
-docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeers --net=containers --name whowantsabeer -d mdillon/postgis
-python -m src.infra.create_db.py
-python -m src.infra.server.py
+docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=containers --name whowantsabeer -d mdillon/postgis
+python -m src.infra.create_db
+python -m tests.generate_db_data
+python -m src.infra.server
