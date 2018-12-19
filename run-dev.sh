32be9a7be11fa8df51fd5ac81f0ef3f79aa11852
Yuri Matheus <yurimatheusb@hotmail.com>
2018-12-19 12:09:03 -0200

    fix the runs sh

--- run-dev.sh
+++ run-dev.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 docker network create -d bridge --subnet 172.16.0.0/24 --gateway 172.16.0.1 beer-dev
-docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=beer-dev --name whowantsabeer -d mdillon/postgis
+docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=beer-dev -d mdillon/postgis
 sleep 10
+pip install -r requirements.txt
 python -m src.infra.create_db
 python -m tests.generate_db_data
 python  server.py
--- run.sh
+++ run.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-export DATABASE_NAME=whowantsabeer
-export DATABASE_USER=app
-export DATABASE_PASSWORD=app
-export DATABASE_HOST=172.16.1.2
-export DEBUG=False
-
-docker network create -d bridge --subnet 172.16.1.0/24 --gateway 172.16.1.1 beer-prod
-docker container run -e POSTGRES_USER=app -e POSTGRES_PASSWORD=app -e POSTGRES_DB=whowantsabeer --net=beer-prod --name whowantsabeer -d mdillon/postgis
-sleep 10
 python -m src.infra.create_db
-docker container run -p 5000:5000 --net=beer-prod --name beer-app -d oyurimatheus/whowantsabeer
+python  server.py
